b3eab5e9efc3ce74f1d6ddbdab70155b0bd2c8d7
tohirul <tohirul.islam164@gmail.com>
2024-11-28 01:02:18 +0600

    [Update] Completed Binary Search [File] BinarySearchLilst.ts, TwoCrystalBalls.ts

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+    do {
+        const midPoint = Math.floor(low + (high - low) / 2);
+        const midValue = haystack[midPoint];
 
+        if (midValue === needle) {
+            return true;
+        } else if (midValue > needle) {
+            high = midPoint;
+        } else {
+            low = midPoint + 1;
+        }
+    } while (low < high);
+    return false;
 }

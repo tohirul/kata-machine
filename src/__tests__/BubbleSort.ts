f83ff23e918404c3098e725a6549d387c160b997
tohirul <tohirul.islam164@gmail.com>
2024-11-28 09:59:11 +0600

    [Date] Nov 28, 2024 [Updte] Complete Bubble Sort [File] BubbleSort.ts

--- src/__tests__/BubbleSort.ts
+++ src/__tests__/BubbleSort.ts
@@ -2,9 +2,10 @@ import bubble_sort from "@code/BubbleSort";
 
 test("bubble-sort", function () {
     const arr = [9, 3, 7, 4, 69, 420, 42];
-
+    const arr2 = [1, 3, 9, 7, 4, 5, 2, 8, 6];
     debugger;
     bubble_sort(arr);
+    bubble_sort(arr2);
     expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+    expect(arr2).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 });
-
--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,17 @@
-export default function bubble_sort(arr: number[]): void {
+function swap(arr: number[], idx1: number, idx2: number): void {
+    const tmp = arr[idx1];
+    arr[idx1] = arr[idx2];
+    arr[idx2] = tmp;
+}
 
+export default function bubble_sort(arr: number[]): void {
+    console.log(arr);
+    for (let i = 0, end = arr.length; i < end; i++) {
+        for (let j = 0; j < end - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+        console.log(`i: ${i}`, arr);
+    }
 }
